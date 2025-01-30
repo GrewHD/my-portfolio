1e23a6c3096948fb7120c66d10cbcdbdecd4c496
Mykyta Vitokhin <vitokhin.nikitaa@gmail.com>
2025-01-30 23:05:05 +1100

    feat: updated AboutMe information

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -5,19 +5,20 @@ export default function AboutMePage() {
                     <img alt="My picture" src="./portrait.png" />
                 </div>
             <ul className="sm:list-disc list-inside w-full text-3xl [&>li>span]:text-white">
-                <li><span>Frameworks:</span> React.js, Svelte(Kit), Vue.js, Next.js, Nuxt.js</li>
-                <li><span>Languages:</span> JavaScript, TypeScript, CSS/SASS/SCSS, HTML5, C++/C#, Python, Golang, SQL, JSON</li>
-                <li><span>Styling & UI:</span> Tailwind CSS, Storybook</li>
+                <li><span>Languages:</span> Javascript/Typescript, Golang, Python </li>
+                <li><span>Frontend:</span> React, Svelte, Vue.js, Javascript/TypeScript, Next.js/Nuxt.js, HTML, CSS/SASS/SCSS, Tailwind, Storybook</li>
+                <li><span>Backend / APIs:</span> Node.js, Express.js, GraphQL, REST, PostgreSQL, MySQL, SQL</li>
                 <li><span>Testing:</span> React/Svelte Testing Library, Vitest, Playwright</li>
-                <li><span>APIs:</span> GraphQL (Apollo), REST</li>
-                <li><span>CMS:</span> Commercetools, Contentful, Hygraph</li>
-                <li><span>DevOps & Cloud:</span> AWS, Vercel</li>
-                <li><span>Database:</span> MySQL</li>
-                <li><span>Version Control & PM:</span> Git, Jira, Trello, Asana, Wrike</li>
+                <li><span>Tools:</span> AWS, Vercel, Contentful, Commercetools, Git, Jira, Trello, Asana, Wrike</li>
             </ul>
             <div className="text-3xl">
                 <p>
-                    I am an experienced Frontend Developer with a focus on accessibility and SEO-friendly practices. Proficient in React, Svelte, TypeScript, JavaScript, Next.js and with expertise in developing design systems with Storybook. Additionally, I have expanded my skill set to include Vue.js development. Successfully delivered various projects throughout my career.
+                    Full Stack Developer with 4+ years of experience in building scalable, high-performance web applications using
+                    modern technologies such as React, Javascript / Typescript, Node.js, and GraphQL. Proven expertise in frontend
+                    development with Svelte, Vue.js, and Next.js, and a background in backend development, API integration, and
+                    database management. Skilled in optimizing application performance, enhancing user experience, and
+                    implementing robust testing and CI/CD pipelines. Collaborative team player with a track record of delivering pixelperfect designs and maintaining brand consistency across e-commerce platforms. Passionate about creating
+                    efficient, maintainable code and continuously improving development processes.
                 </p>
             </div>
             <div className='flex justify-between gap-4 w-full'>
